447748dabba15801d2e8dece21cb2b8eaf6bf962
Duncan <dcduncandc@gmail.com>
2022-06-06 16:10:43 -0400

    started Javascript

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,16 @@
 window.alert("This is an alert! Javascript is running!");
+var playerName = window.prompt("What is your robot's name?");
+// Note the lack of quotation marks aaround playerName
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// This will do math and log 20
+console.log(10 + 10);
+console.log("Our robot's name is " + playerName);
+
+// this creates a function named "fight"
+function fight() {
+  window.alert("The fight has begun!");
+}
+
+// fight();
